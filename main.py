7c8727b389e72500b8ecb3ef739c81bf5f678475
Aviva J J <98533789+afried55@users.noreply.github.com>
2022-03-22 13:51:12 +0000

    list

--- main.py
+++ main.py
@@ -5,7 +5,16 @@ def main():
     pygame.init()
     #Create an instance on your controller object
     #Call your mainloop
+    mylist = []
+    for i in range(4):
+      user = int(input("Enter a number: "))
+      mylist.append(user)
+
+    for i in range(4):
+      print(mylist[i])
+    mylist[0],mylist[3] = mylist[3],mylist[0]
     
+    print(mylist)
     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 # https://codefather.tech/blog/if-name-main-python/
